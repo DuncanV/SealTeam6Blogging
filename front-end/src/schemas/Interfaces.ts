45ad771398e2fde9673ae2da4db10bfc77160e24
Wesley Earl Stander <wesleys@bbd.co.za>
2021-07-02 15:29:24 +0200

    initial

---
+++ front-end/src/schemas/Interfaces.ts
@@ -0,0 +1,36 @@
+import {EPermission, ERole} from "./Enums";
+
+export interface IDeletable {
+  deleted: boolean;
+}
+
+export interface ICreated {
+  created: Date;
+}
+
+export interface IObject extends ICreated, IDeletable {
+  id: string;
+}
+
+export interface IContent extends IObject {
+  userId: string;
+  title: string;
+  likes: string[];
+  content: string;
+}
+
+export interface IUser extends IObject {
+  username: string;
+  passwordHash: string;
+  salt: string;
+  name: string;
+  surname: string;
+  email: string;
+  roles: ERole[];
+}
+
+export interface IRole {
+  key: ERole;
+  permissions: EPermission[];
+}
+
