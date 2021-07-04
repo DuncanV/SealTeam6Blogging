f3a025f3d91b6b6053af5b561e7d2cbd6cf93d99
bradleyp <bradleyp@bbd.co.za>
2021-07-04 22:21:26 +0200

    mongo db setup

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,23 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
-import * as blogRoutes from "./routes/blog";
-import * as userRoutes from "./routes/user";
+import { UserRouter } from "./routes/user";
+import { BlogsRouter } from "./routes/blog";
+import { Mongo } from "./db/dbconfig";
 
 const app = express();
 app.use(express.json());
-dotenv.config({path: __dirname+'./../.env'});
+dotenv.config({ path: __dirname + "./../.env" });
 
-const port = process.env.SERVER_PORT; // default port to listen
+const port = process.env.SERVER_PORT || 3000; // default port to listen
 
-blogRoutes.register(app);
-userRoutes.register(app);
+app.use(UserRouter);
+app.use(BlogsRouter);
 
 app.get("/", (req, res) => {
-    // return website (angular)
-    res.sendStatus(400);
+  // return website (angular)
+  res.sendStatus(400);
 });
 
 // start the Express server
-app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`);
+app.listen(port, async () => {
+  console.log(`server started at http://localhost:${port}`);
+
+  try {
+    await Mongo.connect(`mongodb+srv://sealteam:Se@lTeam123!@sealteam6blogging.jcabz.mongodb.net/SealTeam6Blogging?retryWrites=true&w=majority`);// process.env.MONGO_CONNECTION_URL);
+    const db = process.env.MONGO_DATABASE;
+    console.info(`MongoDB Status: Connected`);
+  } catch (error) {
+    console.error(error);
+  }
 });
