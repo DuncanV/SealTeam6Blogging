5edd489f23d90efa1ad25403de224efc87075ba9
Gerrit Burger <gerrit.burger@standardbank.co.za>
2021-07-06 19:22:52 +0200

    Update HttpServices Front end

--- front-end/src/app/components/blogs/blog-modal/blog-modal.component.ts
+++ front-end/src/app/components/blogs/blog-modal/blog-modal.component.ts
@@ -1,6 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {IContent} from "../../../common/Interfaces";
 import {MAT_DIALOG_DATA} from "@angular/material/dialog";
+import {BlogsService} from "../../../services/blogs.service";
 
 @Component({
   selector: 'app-blog-modal',
@@ -11,6 +12,7 @@ export class BlogModalComponent implements OnInit {
   blog: IContent;
 
   constructor(
+    private blogService: BlogsService,
     @Inject(MAT_DIALOG_DATA) data: any
   ) {
     this.blog = data.blog;
@@ -19,4 +21,7 @@ export class BlogModalComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  deleteBlog(blog: IContent) {
+    this.blogService.deleteBlog(blog);
+  }
 }
