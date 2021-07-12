baafe018e983cd691c9a64b1d65e16c82e30d340
alistairBBD <alistairn@bbd.co.za>
2021-07-12 05:49:17 +0200

    Small functionality that requires you to check 'I am (not) a human' after the first failed login

--- front-end/src/app/components/auth/sign-in/sign-in.component.ts
+++ front-end/src/app/components/auth/sign-in/sign-in.component.ts
@@ -19,6 +19,10 @@ export class SignInComponent implements OnInit, OnDestroy {
   usernameSignIn: string = "";
   passwordSignIn: string = "";
 
+  humanQuestion: boolean = true;
+  humanChecked: boolean = false;
+  firstTimeLogin: boolean = true;
+
   signedIn: boolean | undefined;
 
   private subscriptions = new Subscription();
@@ -86,7 +90,11 @@ export class SignInComponent implements OnInit, OnDestroy {
   }
 
   signIn(): void {
-    this.service.login(this.usernameSignIn, this.passwordSignIn);
+    let autoReject = !(this.firstTimeLogin || this.humanChecked == this.humanQuestion);
+    
+    this.service.login(this.usernameSignIn, this.passwordSignIn, autoReject);
+    this.firstTimeLogin = false;
+    this.humanQuestion = !this.humanQuestion;
   }
 
   signUp(): void {
